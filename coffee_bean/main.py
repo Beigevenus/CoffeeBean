037ad51f27851a1e25cbd1f94477c69bc76d8a6a
Bennett Sloan <55695907+Beigevenus@users.noreply.github.com>
2023-04-02 00:53:55 +0200

    Disabled recommendation functionality temporarily

--- coffee_bean/main.py
+++ coffee_bean/main.py
@@ -10,7 +10,7 @@ intents = discord.Intents(message_content=True, guilds=True)
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-sc = ScoreCalculator()
+# sc = ScoreCalculator()
 
 
 @client.event
@@ -19,22 +19,22 @@ async def on_ready():
     print("Ready!")
 
 
-@tree.command(name="recommend", description="Provides a recommendation of whether to verify a user or not given a "
-                                            "message ID", guild=discord.Object(id=os.getenv("GUILD_ID")))
-async def recommend(interaction, message_id: str):
-    if is_mod(interaction.user, interaction.guild):
-        partial_msg = client.get_partial_messageable(interaction.channel_id)
-
-        try:
-            msg: discord.Message = await partial_msg.fetch_message(int(message_id))
-            recommendation = sc.evaluate(msg.content)
-            await interaction.response.send_message(content=f"```{recommendation}```", ephemeral=True)
-        except discord.errors.NotFound:
-            await interaction.response.send_message(content=f"```ERROR: I wasn't able to find the message with ID "
-                                                            f"{message_id}.```", ephemeral=True)
-    else:
-        await interaction.response.send_message(content="```You don't have permission to use this command!```",
-                                                ephemeral=True)
+# @tree.command(name="recommend", description="Provides a recommendation of whether to verify a user or not given a "
+#                                             "message ID", guild=discord.Object(id=os.getenv("GUILD_ID")))
+# async def recommend(interaction, message_id: str):
+#     if is_mod(interaction.user, interaction.guild):
+#         partial_msg = client.get_partial_messageable(interaction.channel_id)
+#
+#         try:
+#             msg: discord.Message = await partial_msg.fetch_message(int(message_id))
+#             recommendation = sc.evaluate(msg.content)
+#             await interaction.response.send_message(content=f"```{recommendation}```", ephemeral=True)
+#         except discord.errors.NotFound:
+#             await interaction.response.send_message(content=f"```ERROR: I wasn't able to find the message with ID "
+#                                                             f"{message_id}.```", ephemeral=True)
+#     else:
+#         await interaction.response.send_message(content="```You don't have permission to use this command!```",
+#                                                 ephemeral=True)
 
 
 def is_mod(member, guild) -> bool:
