a86b7415968d4915812c0d2fe175a1fd04083570
Bennett Sloan <55695907+Beigevenus@users.noreply.github.com>
2023-04-02 02:35:40 +0200

    Added /verify functionality, but ran into an issue with other bots not reacting to a bot's messages

--- coffee_bean/main.py
+++ coffee_bean/main.py
@@ -38,16 +38,33 @@ async def on_ready():
 
 
 @tree.command(name="verify", description="Verifies a user.", guild=discord.Object(id=os.getenv("GUILD_ID")))
-def verify(interaction, member: discord.Member):
+async def verify(interaction, member: discord.Member):
+    roles = [
+        interaction.guild.get_role(952916636669710367),
+        interaction.guild.get_role(952916837958569994),
+        interaction.guild.get_role(952917220265181214),
+        interaction.guild.get_role(952917088694054932),
+        interaction.guild.get_role(982188895372451870),
+        interaction.guild.get_role(975397798780473344),
+        interaction.guild.get_role(975398729332953129),
+        interaction.guild.get_role(939552461553803345),
+    ]
+
     if is_mod(interaction.user, interaction.guild):
         # Assign roles to user
+        for role in roles:
+            await member.add_roles(role)
+
+        await member.remove_roles(interaction.guild.get_role(939552481791340626))
+
+        await interaction.response.defer()
 
         # Create transcript of ticket
-        await interaction.response.send_message(content="$transcript")
+        await interaction.followup.send(content="$transcript")
 
         # Delete ticket
-        await interaction.response.send_message(content="$close")
-        await interaction.response.send_message(content="$delete")
+        await interaction.followup.send(content="$close")
+        await interaction.followup.send(content="$delete")
 
     else:
         await interaction.response.send_message(content="```You don't have permission to use this command!```",
@@ -55,7 +72,7 @@ def verify(interaction, member: discord.Member):
 
 
 @tree.command(name="reject", description="Rejects a user.", guild=discord.Object(id=os.getenv("GUILD_ID")))
-def reject(interaction):
+async def reject(interaction):
     if is_mod(interaction.user, interaction.guild):
         pass
     else:
