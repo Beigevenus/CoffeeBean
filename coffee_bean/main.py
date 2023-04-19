0d519b1e55d25b06d31842becd5a80f4c648bd1e
Bennett Sloan <55695907+Beigevenus@users.noreply.github.com>
2023-04-19 02:25:35 +0200

    Added skeletons for opt-in and opt-out commands, as well as having changed the desired database structure, which is reflected in the rewritten queries

--- coffee_bean/main.py
+++ coffee_bean/main.py
@@ -3,14 +3,13 @@ import discord
 import sqlite3
 from typing import Literal, get_args
 from dotenv import load_dotenv
-from discord import app_commands
 from queries import *
 
 load_dotenv()
 
 intents = discord.Intents(message_content=True, guilds=True)
 client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
+tree = discord.app_commands.CommandTree(client)
 
 Menu = Literal["Bi-curious Bagel Bites", "Gender-bender Burger", "Grilled Queer-ini", "Non-binary Nachos",
                "Pogayto Salad", "Sapphic Skewers", "Louis' Twinky Tacos", "Nova's Space Dust", "Zoe's Snail Supremo",
@@ -70,15 +69,52 @@ async def find_intro(interaction, member: discord.Member):
 
 
 # RP commands
+@tree.command(name="opt-in", description="Opt-in to all the fun RP stuff!",
+              guild=discord.Object(id=os.getenv("GUILD_ID")))
+async def opt_in(interaction):
+    pass
+
+
+@tree.command(name="opt-out", description="Opt-out of all RP stuff! WARNING: This will erase all your RP data!",
+              guild=discord.Object(id=os.getenv("GUILD_ID")))
+async def opt_out(interaction, confirm: bool = False):
+    if confirm:
+        pass
+    else:
+        await interaction.response.send_message(content=f"WARNING: If you opt out of RP, all of your information will "
+                                                        f"be deleted, including your balance, inventory, etc.\nIf "
+                                                        f"you're sure you wish to do this, please run the `/opt-out` "
+                                                        f"command again with `confirm` set to True.",
+                                                ephemeral=True)
+
+
 @tree.command(name="menu", description="Shows the café's menu.", guild=discord.Object(id=os.getenv("GUILD_ID")))
 async def menu(interaction, format: Literal["image", "text"] = "image"):
-    if format == "card":
-        await interaction.response.send_message(file=discord.File(r"./resources/images/menu.jpg"), ephemeral=True)
-    elif format == "text":
-        await interaction.response.send_message(content=f"Here's a more *digestible* menu:\n{', '.join(menu_list)}",
+    if is_opted_in(interaction.user.id):
+        if format == "card":
+            await interaction.response.send_message(file=discord.File(r"./resources/images/menu.jpg"), ephemeral=True)
+        elif format == "text":
+            await interaction.response.send_message(content=f"Here's a more *digestible* menu:\n{', '.join(menu_list)}",
+                                                    ephemeral=True)
+    else:
+        await interaction.response.send_message(content="You need to opt in to RP before being able to use this"
+                                                        "command. Please refer to the `/opt-in` command",
                                                 ephemeral=True)
 
 # TODO: Implement table threads using Interaction.channel and TextChannel.create_thread()
+
+
+def is_opted_in(member_id: str) -> bool:
+    result = query_db(SELECT_MEMBER_ID, (member_id,))
+
+    if result:
+        if result[0][0] == 0:
+            return False
+        elif result[0][0] == 1:
+            return True
+    else:
+        query_db(INSERT_MEMBER, (member_id,))
+        return False
 
 
 def query_db(query: str, vars: tuple) -> list:
